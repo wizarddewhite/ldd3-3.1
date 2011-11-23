fe2904b7e87c4368de057e5d3ca538147ebaeab0
Richard Yang <weiyang.kernel@gmail.com>
2011-11-23 22:21:00 +0800

    Advanced Char Driver Operations I -- ioctl 1. ioctl command number    number: the ordinal number    type:   the magic number    size:   the size of user data involved    dirction: read or write or both 2. use access_ok() to verify the memory region 3. use capable() to check whether the process has privilege    to call the ioctl 4. one application example of ioctl

---
+++ scull/ioctl_test.c
@@ -0,0 +1,67 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ioctl_test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/21/2011 10:52:00 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/ioctl.h>
+
+
+/*
+ * Ioctl definitions
+ */
+
+/* Use 'k' as magic number */
+#define SCULL_IOC_MAGIC  'k'
+/* Please use a different 8-bit number in your code */
+
+#define SCULL_IOCRESET    _IO(SCULL_IOC_MAGIC, 0)
+
+/*
+ * S means "Set" through a ptr,
+ * T means "Tell" directly with the argument value
+ * G means "Get": reply by setting through a pointer
+ * Q means "Query": response is on the return value
+ * X means "eXchange": switch G and S atomically
+ * H means "sHift": switch T and Q atomically
+ */
+#define SCULL_IOCSQUANTUM _IOW(SCULL_IOC_MAGIC,  1, int)
+#define SCULL_IOCSQSET    _IOW(SCULL_IOC_MAGIC,  2, int)
+#define SCULL_IOCTQUANTUM _IO(SCULL_IOC_MAGIC,   3)
+#define SCULL_IOCTQSET    _IO(SCULL_IOC_MAGIC,   4)
+#define SCULL_IOCGQUANTUM _IOR(SCULL_IOC_MAGIC,  5, int)
+#define SCULL_IOCGQSET    _IOR(SCULL_IOC_MAGIC,  6, int)
+#define SCULL_IOCQQUANTUM _IO(SCULL_IOC_MAGIC,   7)
+#define SCULL_IOCQQSET    _IO(SCULL_IOC_MAGIC,   8)
+#define SCULL_IOCXQUANTUM _IOWR(SCULL_IOC_MAGIC, 9, int)
+#define SCULL_IOCXQSET    _IOWR(SCULL_IOC_MAGIC,10, int)
+#define SCULL_IOCHQUANTUM _IO(SCULL_IOC_MAGIC,  11)
+#define SCULL_IOCHQSET    _IO(SCULL_IOC_MAGIC,  12)
+
+
+int main()
+{
+	int fd;
+	int quantum;
+	fd = open("/dev/scull", O_RDWR);
+	quantum = ioctl(fd, SCULL_IOCQQUANTUM, &quantum);
+	printf ("the quantum size is %d\n", quantum);
+	ioctl(fd, SCULL_IOCGQUANTUM, &quantum);
+	printf ("the quantum size is %d\n", quantum);
+	return 0;
+}
